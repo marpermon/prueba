0d774eea982a7abcdecf247d8887772d7398cc3e
marpermon <maria.perezmonge@ucr.ac.cr>
2023-01-09 18:41:28 -0600

    gato2

--- gatos/gato.py
+++ gatos/gato.py
@@ -6,4 +6,10 @@ gato1.color = "negro"
 gato1.edad = 4
 gato1.aniadirlista()
 
+gato2 = listagatos.gato()
+gato2.nombre = "pelusa"
+gato2.color = "cafe" 
+gato2.edad = 3
+gato2.aniadirlista()
+
 listagatos.printlista()
